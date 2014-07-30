0cba57eb387b5a0a81c38eb2b0cb1d339ba88734
Ryan D'souza <dsouzarc@gmail.com>
2014-07-30 09:19:45 -0400

    Added Popup Menu for start button

--- app/src/main/java/com/ryan/keyboardscrambler/HomeScreen.java
+++ app/src/main/java/com/ryan/keyboardscrambler/HomeScreen.java
@@ -5,32 +5,84 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.PopupMenu.OnMenuItemClickListener;
 import android.view.View;
+import android.view.MenuInflater;
 import android.widget.TextView;
+import android.view.View.OnCreateContextMenuListener;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.widget.PopupMenu;
 
 //TODO: Look up how to make menu float in or look like it's floating
 
 public class HomeScreen extends Activity {
 
     private TextView startTV;
+    private String[] theWords;
+    private PopupMenu thePopup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
 
-        final String[] theWords = getIntent().getExtras().getStringArray("words");
-
+        theWords = getIntent().getExtras().getStringArray("words");
         startTV = (TextView) findViewById(R.id.startTV);
+        thePopup = new PopupMenu(getApplicationContext(), startTV);
+        thePopup.getMenuInflater().inflate(R.menu.choose_level, thePopup.getMenu());
+        thePopup.setOnMenuItemClickListener(new PopupMenuListenerChooseLevel());
+
 
         startTV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final Intent toMedium = new Intent(HomeScreen.this, Medium_Level.class);
-                toMedium.putExtra("words", theWords);
-                startActivity(toMedium);
+                thePopup.show();
             }
         });
+    }
+    public class PopupMenuListenerChooseLevel implements OnMenuItemClickListener {
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+            Intent theIntent;
+            switch (item.getItemId()) {
+                case R.id.easyLevelItem:
+                    theIntent = new Intent(HomeScreen.this, Easy_Level.class);
+                    break;
+                case R.id.mediumLevelItem :
+                    theIntent = new Intent(HomeScreen.this, Medium_Level.class);
+                    break;
+                case R.id.hardLevelItem:
+                    theIntent = new Intent(HomeScreen.this, HardLevel.class);
+                    break;
+                default:
+                    return false;
+            }
+            theIntent.putExtra("words", theWords);
+            startActivity(theIntent);
+            return true;
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        Intent theIntent;
+        switch (item.getItemId()) {
+            case R.id.easyLevelItem:
+                theIntent = new Intent(HomeScreen.this, Easy_Level.class);
+                break;
+            case R.id.mediumLevelItem :
+                theIntent = new Intent(HomeScreen.this, Medium_Level.class);
+                break;
+            case R.id.hardLevelItem:
+                theIntent = new Intent(HomeScreen.this, HardLevel.class);
+                break;
+            default:
+                return super.onContextItemSelected(item);
+        }
+        theIntent.putExtra("words", theWords);
+        startActivity(theIntent);
+        return true;
     }
 
     @Override
