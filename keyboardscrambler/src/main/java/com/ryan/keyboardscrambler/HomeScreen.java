9be359515b1736f0a14925b908d88a8d6a1b7f29
Ryan D'souza <dsouzarc@gmail.com>
2014-08-05 08:48:55 -0400

    Attempted to change PopupMenu text color (no success)

--- keyboardscrambler/src/main/java/com/ryan/keyboardscrambler/HomeScreen.java
+++ keyboardscrambler/src/main/java/com/ryan/keyboardscrambler/HomeScreen.java
@@ -31,7 +31,6 @@ public class HomeScreen extends Activity {
 
         theWords = getIntent().getExtras().getStringArray("words");
         startTV = (TextView) findViewById(R.id.startTV);
-        //thePopup = new PopupMenu(HomeScreen.this, startTV);
         thePopup = new PopupMenu(HomeScreen.this, findViewById(R.id.locationForChooseGameMenu));
 
         thePopup.getMenuInflater().inflate(R.menu.choose_level, thePopup.getMenu());
