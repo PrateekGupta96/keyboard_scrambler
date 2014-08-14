0676132611b23a221b23e984863dfed3518278d5
Ryan D'souza <dsouzarc@gmail.com>
2014-08-14 07:39:50 -0400

    5th row

--- keyboardscrambler/src/main/java/com/ryan/keyboardscrambler/Dvorak_Level.java
+++ keyboardscrambler/src/main/java/com/ryan/keyboardscrambler/Dvorak_Level.java
@@ -6,17 +6,63 @@ import android.view.Menu;
 import android.view.MenuItem;
 import com.ryan.keyboardscrambler.R;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
 public class Dvorak_Level extends KeyboardLevel {
 
-    private static final String theStr = "1234567890" + DELETE_CHAR + "\",.pyfgcrl?=\\aoeuidhtns:qjkxbmwvz";
-    private static final String punctuation = "\",.?=\\:";
+    private static final String theStr = "~1234567890" + DELETE_CHAR + "\",.pyfgcrl?=\\aoeuidhtns:qjkxbmwvz";
+    private static final String punctuation = "\",.?=\\:~";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dvorak__level);
 
+        this.LEVEL = Level.DVORAK;
 
+        firstRow = (LinearLayout) findViewById(R.id.firstRow);
+        secondRow = (LinearLayout) findViewById(R.id.secondRow);
+        thirdRow = (LinearLayout) findViewById(R.id.thirdRow);
+        fourthRow = (LinearLayout) findViewById(R.id.fourthRow);
+
+        //Shows characters user pressed
+        userResponse = (TextView) findViewById(R.id.userResponseTV);
+        userResponse.setCursorVisible(true);
+
+        timeTV = (TextView) findViewById(R.id.timeTV);
+        setUpTimer(timeTV);
+
+        //Scrambled version of the str
+        final String reArranged = this.theStr;
+        int counter = 0;
+
+        //Add the keys to the screen
+        for(byte i = 0; i < 14; i++, counter++)
+            firstRow.addView(getCharTV(reArranged.charAt(counter), 14));
+        for(byte i = 0; i < 13; i++, counter++)
+            secondRow.addView(getCharTV(reArranged.charAt(counter), 13));
+        for(byte i = 0; i < 10; i++, counter++)
+            thirdRow.addView(getCharTV(reArranged.charAt(counter), 10));
+        for(byte i = 0; i < 10; i++, counter++)
+            fourthRow.addView(getCharTV(reArranged.charAt(counter), 10));
+
+        //Refresh button --> DEV PURPOSES
+        refresh = (TextView) findViewById(R.id.refresh);
+        refresh.setText(getLevelWord());
+        refresh.setTextColor(Color.parseColor("#ff0099cc"));
+        refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recreate();
+            }
+        });
 
 
     }
